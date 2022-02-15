078953e0768dbe71f445e0fb87bf60fcb866141d
Macwel <wifi-215@yandex.ru>
2022-02-15 19:17:46 +0300

    add login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,13 @@
+import { LoginContainers } from '@containers/login/Login'
+/**
+ * Homepage
+ */
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <LoginContainers />
+    </>
+  )
+}
+
+export default HomePage
